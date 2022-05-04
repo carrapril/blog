de934e80358b522a5095ccedbfeb9339f981f047
carrapril <97204002+carrapril@users.noreply.github.com>
2022-05-04 19:59:49 -0400

    Add files via upload

---
+++ posts/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.urls import reverse
+
+class Post(models.Model):
+    title = models.CharField(max_length=256)
+    author = models.ForeignKey(
+        'auth.User', 
+        on_delete=models.CASCADE,
+        null=True, default=None
+    )
+    body = models.TextField()
+    created_on = models.DateTimeField(
+        auto_now_add=True,
+        null=True,
+        default=None
+        )
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('post_detail', args=[self.id])
